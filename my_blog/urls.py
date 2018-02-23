aa260564c93a17f0b79e656421ef13b87f9d5f24
fengbin <fengbin.dev@outlook.com>
2018-02-23 21:13:26 +0800

    my update

--- my_blog/urls.py
+++ my_blog/urls.py
@@ -13,17 +13,60 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# from django.conf.urls import url
+# from django.contrib import admin
+# from article import views
+
+# urlpatterns = [
+#     url(r'^admin/', admin.site.urls),
+#     # url(r'^$',views.home),
+#     # url(r'^(?P<my_args>\d+)/$',views.detail,name = 'detail'),
+#     # url(r'^test/$',views.test),
+#     url(r'^$',views.home,name = 'home'),
+#     url(r'^(P<id>\d+)/$',views.detail,name='detail'),
+#
+# ]
+
+"""
 from django.conf.urls import url
 from django.contrib import admin
 from article import views
 
 urlpatterns = [
+    # Examples:
+    # url(r'^$', 'my_blog.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
     url(r'^admin/', admin.site.urls),
-    # url(r'^$',views.home),
-    # url(r'^(?P<my_args>\d+)/$',views.detail,name = 'detail'),
-    # url(r'^test/$',views.test),
-    url(r'^$',views.home),
+    url(r'^$', views.home, name = 'home'),
+    url(r'^(?P<id>\d+)/$', views.detail, name='detail'),
+    url(r'^archives/$',views.archives,name='archives'),
+    url(r'^aboutme/$',views.about_me,name='about_me'),
+    url(r'^tag(?P<tag>\w+)/$', views.search_tag, name = 'search_tag'),
+
+
 ]
+
+"""
+
+from django.conf.urls import url
+from django.contrib import admin
+from article import views
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'my_blog.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.home, name = 'home'),
+    url(r'^(?P<id>\d+)/$', views.detail, name='detail'),
+    url(r'^archives/$', views.archives, name = 'archives'),
+    url(r'^aboutme/$', views.about_me, name = 'about_me'),
+    url(r'^tag(?P<tag>\w+)/$', views.search_tag, name = 'search_tag'),
+
+]
+
 
 
 
